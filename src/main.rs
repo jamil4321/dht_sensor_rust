1dc8d535c4059fce12f91f7f234914f98097dae1
jamil4321 <jamil.ahmed.ja28@gmail.com>
2020-04-26 22:37:43 +0500

    done 80% run in release mode

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@
 extern crate panic_itm; // panic handler
 use cortex_m::{iprint, iprintln, peripheral::ITM};
 use cortex_m_rt::entry;
-use f3::hal::{gpio,time::MonoTimer,delay::Delay};
-use f3::hal::{prelude::*,stm32f30x::{self,GPIOA,RCC,TIM6,rcc,tim16}};
-use heapless::Vec;
-use heapless::consts::*;
+use f3::hal::{time::MonoTimer,delay::Delay};
+use f3::hal::{prelude::*,stm32f30x::{self,GPIOA}};
 
 
 #[entry]
@@ -22,9 +20,8 @@ fn main()->!{
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     let mut delay = Delay::new(cp.SYST, clocks);
-    let mono_time = MonoTimer::new(cp.DWT, clocks);
     let mut itm = cp.ITM;
-    delay.delay_ms(1000_u32);
+    delay.delay_ms(1500_u32);
     iprintln!(&mut itm.stim[0],"waiting for");
     let mut gpioa = dp.GPIOA.split(& mut rcc.ahb);
     let mut pa3 = gpioa.pa3.into_open_drain_output(&mut gpioa.moder,&mut gpioa.otyper);
@@ -32,14 +29,24 @@ fn main()->!{
     pa3.set_high();
     delay.delay_us(40_u32);
     pa3.set_low();
-    delay.delay_ms(20_u32);
+    delay.delay_us(20000_u32);
     pa3.set_high();
     pa3.into_pull_up_input(&mut gpioa.moder,&mut gpioa.pupdr);
 
     let mut hum_int = response(&mut delay);
-    let hum_float = response(&mut delay);
+    let mut hum_float = response(&mut delay);
+    let mut temp_int = response(&mut delay);
+    let mut temp_float = response(&mut delay);
+    let mut check_sum = response(&mut delay);
 
-    iprintln!(&mut itm.stim[0],"hum vec {:?}",convert_bit(&mut hum_int));
+    iprintln!(&mut itm.stim[0],"hum data  {:?}.{:?}%",convert_bit(&mut hum_int),convert_bit(&mut hum_float));
+    iprintln!(&mut itm.stim[0],"temp data {:?}.{:?}",convert_bit(&mut temp_int),convert_bit(&mut temp_float));
+    iprintln!(&mut itm.stim[0],"check sum {:?}",convert_bit(&mut check_sum));
+
+
+    // iprintln!(&mut itm.stim[0],"hum data  {:?}.{:?}%", hum_int,hum_float);
+    // iprintln!(&mut itm.stim[0],"temp data {:?}.{:?}",temp_int, temp_float);
+    // iprintln!(&mut itm.stim[0],"check sum {:?}",check_sum);
     loop{}
 }
 
@@ -55,26 +62,26 @@ fn clear_bit()->bool{
         gpioa_pin.idr.read().idr3().bit_is_clear()
     }
 }
-fn response(delay:&mut Delay)->Vec<u8,heapless::consts::U8>{
-    let mut data = Vec::new();
+fn response(delay:&mut Delay)->[u8;8]{
+    let mut data = [0u8;8];
     let delay = delay;
-    for i in 0..8{
+    for byte in data.iter_mut(){
         while clear_bit(){}
         delay.delay_us(35_u32);
         if set_bit(){
-            data.push(1).is_err();
+            *byte = 1
         }else{
-            data.push(0).is_err();
+            *byte = 0;
         }
-        while set_bit(){}
+         while set_bit(){}
+         //delay.delay_us(20_u32);
     }
     data
 }
+fn convert_bit(data:&mut[u8;8]) -> u8{
 
-fn convert_bit(data:&mut Vec<u8,heapless::consts::U8>) -> u8{
-
-    let mut arr = [128,64,32,16,8,4,2,1];
-    let mut vec = data;
+    let arr = [128,64,32,16,8,4,2,1];
+    let vec = data;
     let mut int = 0;
     for i in 0..8{
         int = int + arr[i]*vec[i]
