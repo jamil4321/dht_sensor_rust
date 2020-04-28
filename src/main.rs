253cc2e473d75395c7a3f4319c9a87bd1c8e2565
jamil4321 <jamil.ahmed.ja28@gmail.com>
2020-04-29 03:03:39 +0500

    created DHT 11 Function but No luck in loop and Programme only runs in release mode

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use cortex_m::{iprint, iprintln, peripheral::ITM};
 use cortex_m_rt::entry;
 use f3::hal::{time::MonoTimer,delay::Delay};
 use f3::hal::{prelude::*,stm32f30x::{self,GPIOA}};
-
+use f3::hal::rcc::Rcc;
+use f3::hal::gpio::*;
 
 #[entry]
 fn main()->!{
@@ -21,35 +22,29 @@ fn main()->!{
 
     let mut delay = Delay::new(cp.SYST, clocks);
     let mut itm = cp.ITM;
-    delay.delay_ms(1000_u32);
-    iprintln!(&mut itm.stim[0],"waiting for");
     let mut gpioa = dp.GPIOA.split(& mut rcc.ahb);
     let mut pa3 = gpioa.pa3.into_open_drain_output(&mut gpioa.moder,&mut gpioa.otyper);
     pa3.internal_pull_up(&mut gpioa.pupdr,true);
-    // pa3.set_high();
-    // delay.delay_us(40_u32);
-    pa3.set_low();
-    delay.delay_ms(18u32);
-    pa3.set_high();
-    pa3.into_pull_up_input(&mut gpioa.moder,&mut gpioa.pupdr);
-    delay.delay_us(200u32);
-    let mut hum_int = response(&mut delay);
-    let mut hum_float = response(&mut delay);
-    let mut temp_int = response(&mut delay);
-    let mut temp_float = response(&mut delay);
-    let mut check_sum = response(&mut delay);
-
-    iprintln!(&mut itm.stim[0],"hum data  {:?}.{:?}%",convert_bit(&mut hum_int),convert_bit(&mut hum_float));
-    iprintln!(&mut itm.stim[0],"temp data {:?}.{:?}",convert_bit(&mut temp_int),convert_bit(&mut temp_float));
-    iprintln!(&mut itm.stim[0],"check sum {:?}",convert_bit(&mut check_sum));
+   
+    // pa3.into_pull_up_input(&mut gpioa.moder,&mut gpioa.pupdr);
+    
+    // delay.delay_us(200u32);
 
 
-    iprintln!(&mut itm.stim[0],"hum data  {:?}", hum_int);
-    iprintln!(&mut itm.stim[0],"hum data  {:?}", hum_float);
-    iprintln!(&mut itm.stim[0],"temp data {:?}",temp_int);
-    iprintln!(&mut itm.stim[0],"temp data {:?}",temp_float);
-    iprintln!(&mut itm.stim[0],"check sum {:?}",check_sum);
-    loop{}
+
+    loop{
+    delay.delay_ms(1000_u32);
+    iprintln!(&mut itm.stim[0],"waiting for");
+    dht11(&mut delay, &mut pa3,&mut itm);
+    // pa3.set_low();
+    if set_bit(){
+            iprintln!(&mut itm.stim[0],"bit is set",);
+            
+        }
+        else{
+            iprintln!(&mut itm.stim[0],"bit is clear after",)
+        };
+    }
 }
 
 fn set_bit()->bool{
@@ -89,4 +84,39 @@ fn convert_bit(data:&mut[u8;8]) -> u8{
         int = int + arr[i]*vec[i]
     }
     int
+}
+
+fn dht11 (delay:&mut Delay,pin: &mut f3::hal::gpio::gpioa::PA3<f3::hal::gpio::Output<f3::hal::gpio::OpenDrain>>,itm : &mut ITM)  
+{
+    let mut delay = delay;
+    let mut pa3 = pin; 
+    pa3.set_high();
+    iprintln!(&mut itm.stim[0],"Pin is high");
+    pa3.set_low();
+    delay.delay_ms(18u32);
+    pa3.set_high();
+    iprintln!(&mut itm.stim[0],"Pin is high again");
+    while set_bit(){}
+    while clear_bit(){}
+    while set_bit(){}
+    iprintln!(&mut itm.stim[0],"wait for data");
+    let mut hum_int = response(&mut delay);
+    let mut hum_float = response(&mut delay);
+    let mut temp_int = response(&mut delay);
+    let mut temp_float = response(&mut delay);
+    let mut check_sum = response(&mut delay);
+    delay.delay_us(100000u32);
+    pa3.set_low();
+
+    iprintln!(&mut itm.stim[0],"hum data  {:?}.{:?}%",convert_bit(&mut hum_int),convert_bit(&mut hum_float));
+    iprintln!(&mut itm.stim[0],"temp data {:?}.{:?}",convert_bit(&mut temp_int),convert_bit(&mut temp_float));
+    iprintln!(&mut itm.stim[0],"check sum {:?}",convert_bit(&mut check_sum));
+    iprintln!(&mut itm.stim[0],"hum data  {:?}", hum_int);
+    iprintln!(&mut itm.stim[0],"hum data  {:?}", hum_float);
+    iprintln!(&mut itm.stim[0],"temp data {:?}",temp_int);
+    iprintln!(&mut itm.stim[0],"temp data {:?}",temp_float);
+    iprintln!(&mut itm.stim[0],"check sum {:?}",check_sum);
+
+    // pa3.set_low();
+    // while set_bit(){};
 }
