0e1abb2e20c4b406e8a121f9db54dc6c43fac676
jamil4321 <jamil.ahmed.ja28@gmail.com>
2020-05-01 01:37:03 +0500

    abc

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 #![no_std]
-
+#![no_main]
 
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust53964
 extern crate panic_itm; // panic handler
 use cortex_m::{iprint, iprintln, peripheral::ITM};
+use cortex_m_rt::entry;
 use f3::hal::{time::MonoTimer,delay::Delay};
 use f3::hal::{prelude::*,stm32f30x::{self,GPIOA,RCC}};
 
 
+#[entry]
+fn main()->!{
 
-pub fn dht11 (delay:&mut Delay,pin: &mut f3::hal::gpio::gpioa::PA3<f3::hal::gpio::Output<f3::hal::gpio::OpenDrain>>,itm : &mut ITM)  
-->(u8,u8,u8,u8) {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+    let mut itm = cp.ITM;
+    let mut gpioa = dp.GPIOA.split(& mut rcc.ahb);
+    let mut pa3 = gpioa.pa3.into_open_drain_output(&mut gpioa.moder,&mut gpioa.otyper);
+    pa3.internal_pull_up(&mut gpioa.pupdr,true);
+
+    loop{
+    iprintln!(&mut itm.stim[0],"Wait for data 2 Sec");
+    delay.delay_ms(2000_u32);
+    // dht11(&mut delay, &mut pa3,&mut itm);
+    iprintln!(&mut itm.stim[0],"Wait for data");
+    dht11(&mut delay, &mut pa3,&mut itm);
+
+    }
+}
+
+
+fn dht11 (delay:&mut Delay,pin: &mut f3::hal::gpio::gpioa::PA3<f3::hal::gpio::Output<f3::hal::gpio::OpenDrain>>,itm : &mut ITM)  
+{
     let mut delay = delay;
     let mut pa3 = pin; 
     pa3.set_high();
@@ -18,7 +45,6 @@ pub fn dht11 (delay:&mut Delay,pin: &mut f3::hal::gpio::gpioa::PA3<f3::hal::gpio
     delay.delay_ms(18u32);
     pa3.set_high();
     while set_bit(){}
-
     while clear_bit(){}
     while set_bit(){}
     let mut hum_int = response(&mut delay);
@@ -26,8 +52,9 @@ pub fn dht11 (delay:&mut Delay,pin: &mut f3::hal::gpio::gpioa::PA3<f3::hal::gpio
     let mut temp_int = response(&mut delay);
     let mut temp_float = response(&mut delay);
     let mut check_sum = response(&mut delay);
+    iprintln!(&mut itm.stim[0],"Tempreture {}*C and Humadity {}%",convert_bit(&mut temp_int),convert_bit(&mut hum_int));
 
-    (convert_bit(&mut hum_int),convert_bit(&mut hum_float),convert_bit(&mut temp_int),convert_bit(&mut temp_float))
+    // (convert_bit(&mut hum_int),convert_bit(&mut hum_float),convert_bit(&mut temp_int),convert_bit(&mut temp_float))
 }
 
 
